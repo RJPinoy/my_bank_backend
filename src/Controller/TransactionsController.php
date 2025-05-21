ad03c8df7903dacda7e8fa234dfc9c152beea91e
Rejen <rejenkiller@live.fr>
2025-05-21 16:50:27 +0200

    :feat: Added get Transactions

--- src/Controller/TransactionsController.php
+++ src/Controller/TransactionsController.php
@@ -5,15 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+
+use App\Repository\TransactionsRepository;
 
 final class TransactionsController extends AbstractController
 {
-    #[Route('/transactions', name: 'app_transactions')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TransactionsController.php',
-        ]);
+    #[Route('/api/transactions', name: 'get_transactions', methods: ['GET'])]
+    public function getTransactionList(
+        TransactionsRepository $transactionsRepository, 
+        SerializerInterface $serializerInterface
+    ): JsonResponse {
+        $transactionList = $transactionsRepository->findAll();
+        $jsonTransactionList = $serializerInterface->serialize($transactionList, 'json');
+
+        return new JsonResponse(
+            $jsonTransactionList,
+            Response::HTTP_OK,
+            [],
+            true
+        );
     }
 }
